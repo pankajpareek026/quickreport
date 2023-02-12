ef7c5bc03e96b4f9828acca5ae912a0413228fd2
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2023-02-12 09:49:09 +0530

    connection time out error solved

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,12 @@
 
 //database connection file 
 var mysql = require('mysql2');
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
     host: "sql.freedb.tech",
     user: "freedb_quickreport",
     password: "fUAde$SfZ228N53",
     database: "freedb_main-master",
-    connectTimeout: 288000
-    // debug: true
-
+    connectTimeout:28800
 })
-// file where 
-// CREATE TABLE newUsers(username varchar(50),name varchar(50),emailid varchar(50), sts int,password varchar(50),JoiningDAte varchar(100));
-// conn.connect((err) => {
 
-//     if (err) console.log(err)
-//     else console.log("connected")
-// })
-// const q = ` CREATE TABLE newUsers(username varchar(50),name varchar(50),emailid varchar(50), sts int,password varchar(50),JoiningDAte varchar(100));`
-// conn.query("show tables", (err, result) => {
-//     if (err) console.log(err)
-//     console.log(result)
-// })
-module.exports = conn; //to make variable importable 
+module.exports = conn;  
