48df99952c7ac2d1a88da3e49d7843f63e29c357
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2024-05-01 10:00:49 +0000

    db config change

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 
 //database connection file 
+const env = require('dotenv');
+env.config()
+const fs = require('fs');
 var mysql = require('mysql2');
 var conn = mysql.createPool({
-    host: "sql.freedb.tech",
-    user: "freedb_quickreport",
-    password: "fUAde$SfZ228N53",
-    database: "freedb_main-master",
-    connectTimeout:28800
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(__dirname + '/ssl.pem')
+    },
+    connectTimeout: 25000
+
 })
+
 
 module.exports = conn;  
